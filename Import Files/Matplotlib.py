bfdd58826c990baada664568621b959cf6be2df5
Chanith Adikari <amradikari67@gmail.com>
2025-07-13 22:22:55 +0530

    Create Matplotlib.py

---
+++ Import Files/Matplotlib.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+# First plot (Bar chart)
+plt.bar(['a', 'b', 'c'], [5, 2, 4])
+plt.show()
+
+# Second plot (Line chart)
+plt.plot([2, 40, 80], [10, 2, 7])
+plt.show()
