9bd892deb1413c02b7f0339e49234917eb1560fe
Chanith Adikari <amradikari67@gmail.com>
2025-07-19 23:08:58 +0530

    Create df.py

---
+++ DataFrames/df.py
@@ -0,0 +1,14 @@
+#To reference columns of a dataframe, use square bracket notation:
+#df['column_name']
+
+import pandas as pd
+
+df = pd.DataFrame()
+
+df['color']
+
+df['radius'].min( )
+
+df['diameter'].sum( )
+
+df['radius'].mean( )
